54d26014bcfd2193e24d32f3e655a59a9f3c56de
Leonid_Lobanov <leonidlobanov.es@gmail.com>
2023-12-17 22:54:07 +0100

    adding 1test.py, base_page.py, requirements.txt, conftest.py

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -12,3 +12,9 @@ class TextBoxPageLocators:
     CREATED_EMAIL = (By.CSS_SELECTOR, "#output #email")
     CREATED_CURRENT_ADDRESS = (By.CSS_SELECTOR, "#output #currentAddress")
     CREATED_PERMANENT_ADDRESS = (By.CSS_SELECTOR, "#output #permanentAddress")
+
+
+class CheckBoxPageLocators:
+
+    EXPAND_ALL_BUTTON = (By.CSS_SELECTOR, "button[title='Expand all']")
+    ITEM_LIST = (By.CSS_SELECTOR, "span[class='rct-checkbox']")
