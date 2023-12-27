5e27dfa5bc778599f0a498b231fef9d02fccd716
Leonid_Lobanov <leonidlobanov.es@gmail.com>
2023-12-28 00:07:32 +0100

    adding new to the base_page.py, element_test.py, elements_page.py and elements_page_locators.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -7,6 +7,9 @@ class BasePage:
 
     def open(self):
         self.driver.get(self.url)
+        self.driver.execute_script("document.getElementsByTagName('footer')[0].remove();")
+        self.driver.execute_script("document.getElementById('fixedban').style.display = 'none'")
+
 
     def element_is_visible(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
